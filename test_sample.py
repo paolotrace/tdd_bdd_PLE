5ae7c0464bb7281397105bff09b8c3877a58a5a2
Paolo Perez <paolo.perez@globe.com.ph>
2022-07-21 09:18:42 +0000

    Lab_1_&_2

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+# increment_by_one(i):
+#    return i + 1
+
+#def test_answer():
+#    assert increment_by_one(5) == 2
+    
+def decrement_by_one(i):
+    i = i - 1
+    return i
+
+def test_answer():
+    assert decrement_by_one(3) == 2
